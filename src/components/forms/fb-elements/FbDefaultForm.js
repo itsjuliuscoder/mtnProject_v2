536a9a0ac8553eaafd52f9fd79df14e29c638573
Julius Olamide <juliusolajumokecoder@gmail.com>
2022-09-22 00:45:07 +0100

    consumed the transaction service from the backend api

--- src/components/forms/fb-elements/FbDefaultForm.js
+++ src/components/forms/fb-elements/FbDefaultForm.js
@@ -19,6 +19,10 @@ import CustomRadio from '../custom-elements/CustomRadio';
 import CustomFormLabel from '../custom-elements/CustomFormLabel';
 import { PaystackButton } from 'react-paystack'; 
 import styles from "../../../../styles/Component.module.css";
+import MoonLoader from "react-spinners/MoonLoader";
+import axios from "axios";
+import ErrorToaster from "../../dashboard/dashboard1/ErrorToaster"
+import SuccessToaster from "../../dashboard/dashboard1/SuccessToaster";
 
 const FbDefaultForm = ({ data }) => {
   const [state, setState] = React.useState({
@@ -33,18 +37,72 @@ const FbDefaultForm = ({ data }) => {
 
   const [value, setValue] = React.useState('');
   const [amount, setAmount] = React.useState('');
+  const [ isloading, setIsloading ] = React.useState(false);
+  const [ response, setResponse ] = useState('');
+  const [ errorResponse, setErrorResponse ] = useState('');
+  // const [config, setConfig] = React.useState({
+  //   reference: (new Date()).getTime().toString(),
+  //   email: data && data.email ? data.email : "techcapacitybuilder@gmail.com",
+  //   amount: amount ? amount : "1000",
+  //   publicKey: 'pk_test_cdbf19c426a4d163dd3e939e53edde7f831fd6b6',
+  // });
 
-  const config = {
-    reference: (new Date()).getTime().toString(),
-    email: data && data.email ? data.email : "techcapacitybuilder@gmail.com",
-    amount: amount ? amount : "100",
-    publicKey: 'pk_test_cdbf19c426a4d163dd3e939e53edde7f831fd6b6',
-  };
+  // const config = {
+  //   reference: (new Date()).getTime().toString(),
+  //   email: data && data.email ? data.email : "techcapacitybuilder@gmail.com",
+  //   amount: amount ? amount : "100",
+  //   publicKey: 'pk_test_cdbf19c426a4d163dd3e939e53edde7f831fd6b6',
+  // };
 
   const handlePaystackSuccessAction = (reference) => {
     // Implementation for whatever you want to do with reference and after success call.
     console.log(reference);
+
+    const headers = {
+      Accept: "application/json",
+      Authorization: accessToken ? accessToken : "No Auth"
+    }
+
+    const body = {
+      reference_id: reference,
+      user_id: data._id,
+      fullname: data.firstname + " " + data.lastname,
+      phone_number: data.phone_number,
+      amount: amount,
+      previous_balance: data && data.wallet_balance ? data.wallet_balance : " ",
+      description: "Wallet Top up with amount " + amount + " was successful"
+    }
+    
+    axios({
+      method: 'post',
+      url: 'https://mtn-backend-api-service.herokuapp.com/v1/auth/wallet_transaction',
+      headers,
+      data: body
+    }).then(function(response){
+        console.log("this is the response data -->", response.data);
+        setIsloading(false);
+        if(response.data.statusCode === "000"){
+          setResponse(response.data.statusMessage);
+        } else {
+          console.log("this is the response gotten", response);
+          setErrorResponse("Unable to create PIN");
+        }
+    }).catch((error) => {
+        setIsloading(false);
+        console.log("this is the error response gotten");
+        setErrorResponse("Wallet Top Up failed");
+        setTimeout(setEmptyAlert, 5000);
+    })
   };
+
+  React.useEffect(() =>{
+    setTimeout(() => setIsloading(false), 6000);
+  });
+
+  const setEmptyAlert = () => {
+    setResponse("");
+    setErrorResponse("");
+  }
 
   // you can call this function anything
   const handlePaystackCloseAction = () => {
@@ -52,8 +110,17 @@ const FbDefaultForm = ({ data }) => {
     console.log('closed')
   }
 
+  const handleSubmit = e => { 
+      console.log("this is the amount -->", amount)
+
+
+  }
+
   const componentProps = {
-    ...config,
+    amount: amount ? (amount * 100) : "100", 
+    reference: (new Date()).getTime().toString(),
+    email: data && data.email ? data.email : "techcapacitybuilder@gmail.com",
+    publicKey: 'pk_test_cdbf19c426a4d163dd3e939e53edde7f831fd6b6',
     text: 'Paystack Button Implementation',
     onSuccess: (reference) => handlePaystackSuccessAction(reference),
     onClose: handlePaystackCloseAction,
@@ -100,77 +167,88 @@ const FbDefaultForm = ({ data }) => {
             padding: '30px',
           }}
         >
-          <form>
-          <Grid>
-              <Grid item xs={12} md={6}>
-                <CustomFormLabel
-                  sx={{
-                    mt: 0,
-                  }}
-                  htmlFor="default-value"
-                >
-                  Fullname
-                </CustomFormLabel>
-                <CustomTextField
-                  id="default-value"
-                  variant="outlined"
-                  defaultValue={data && data.firstname ? (data.firstname + " " + data.lastname) : ""}
-                  fullWidth
-                  size="small"
-                  disabled
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                  <CustomFormLabel htmlFor="email-text">Email</CustomFormLabel>
-                  <CustomTextField
-                    id="email-text"
-                    type="email"
-                    variant="outlined"
-                    defaultValue={data && data.email ? (data.email + " " + data.email) : "techcapacitybuilder@gmail.com"}
-                    fullWidth
-                    size="small"
+          {/* <form method="post"> */}
+                <Grid xs={12} lg={4}>
+                  <CustomFormLabel htmlFor="email">Email Address</CustomFormLabel>
+                  <CustomTextField 
+                    type="text"
+                    // placeholder="Enter Phone Number"
+                    name="email" 
+                    id="email" 
+                    variant="outlined" 
+                    fullWidth 
+                    required 
+                    value={data && data.email ? data.email : "techcapacitybuilder@gmail.com"}
                     disabled
+                    // onChange={e => setPhonenumber(e.target.value)} 
+                    />
+                </Grid>
+                <Grid xs={12} lg={4}>
+                  <CustomFormLabel htmlFor="email">Phone Number</CustomFormLabel>
+                  <CustomTextField 
+                    type="phone_number"
+                    // placeholder="Enter Phone Number"
+                    name="email" 
+                    id="email" 
+                    variant="outlined" 
+                    fullWidth 
+                    required 
+                    value={data && data.phone_number ? data.phone_number : "09025015566"}
+                    disabled
+                    // onChange={e => setPhonenumber(e.target.value)} 
+                    />
+                </Grid>
+                <Grid xs={12} lg={4}>
+                  <CustomFormLabel htmlFor="password">Enter Amount</CustomFormLabel>
+                  <CustomTextField
+                    name="amount"
+                    placeholder="Enter Amount"
+                    id="amount"
+                    type="tel"
+                    variant="outlined"
+                    fullWidth
+                    value={amount}
+                    onChange={e => setAmount(e.target.value)}
+                    required
+                    sx={{
+                      mb: 3,
+                    }}
                   />
-              </Grid>
-          </Grid>
-            <Grid item xs={12} md={6}>
-              <CustomFormLabel htmlFor="default-outlined-password-input">Phone Number</CustomFormLabel>
-              <CustomTextField
-                id="default-outlined-password-input"
-                type="number"
-                variant="outlined"
-                defaultValue={data && data.phone_number ? data.phone_number : "09025015566"}
-                fullWidth
-                size="small"
-                disabled
-              />
-            </Grid>            
-            <Grid item xs={12} md={6}>
-              <CustomFormLabel htmlFor="default-outlined-password-input">Enter Amount</CustomFormLabel>
-              <CustomTextField
-                name="amount"
-                placeholder="Enter Amount"
-                id="amount"
-                type="number"
-                variant="outlined"
-                fullWidth
-                size="small"
-                value={amount}
-                onChange={e => setAmount(e.target.value)}
-                required
-
-              />
-            </Grid>
-            <div>
-              {/* <Button sx={{
-                mt: 2,
-              }} color="primary" variant="contained">
-                Submit
-              </Button> */}
-              <PaystackButton className={styles.paystack__button} {...componentProps} />
-            </div>
-          </form>
+                </Grid>
+                <Box
+                  sx={{
+                    display: {
+                      xs: "block",
+                      sm: "flex",
+                      lg: "flex",
+                    },
+                    alignItems: "center",
+                  }}
+                >
+                </Box>
+                {/* <NextLink href="/"> */}
+                <Grid xs={12} lg={12}>
+                  {/* <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    size="large"
+                    fullWidth
+                    sx={{
+                      pt: "10px",
+                      pb: "10px",
+                    }}
+                    onClick={() => setIsloading(!isloading)}
+                  >
+                    {isloading ? <MoonLoader color="#fff" loading={isloading} size={30} /> : 'Top Up' }
+                  </Button> */}
+                  <PaystackButton className={styles.paystack__button} {...componentProps} />
+                </Grid>
+                {/* </NextLink> */}
+          {/* </form> */}
         </CardContent>
+        { errorResponse && <ErrorToaster title={ errorResponse } /> }
+        { response && <SuccessToaster title={ response } /> }
       </Card>
     </div>
   );
